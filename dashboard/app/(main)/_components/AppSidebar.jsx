f828b02af2e84405bc78cad8c0feb5a73a7135f7
Mehul Sweeti Agrawal <148802868+msagr@users.noreply.github.com>
2025-09-30 22:32:16 +0530

    Delete dashboard directory

--- dashboard/app/(main)/_components/AppSidebar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function AppSidebar() {
-    return (
-        <div>App Sidebar</div>
-    );
-};
-
-export default AppSidebar;
--- dashboard/app/(main)/provider.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function DashboardProvider({ children }) {
-    return (
-        <div>{children}</div>
-    );
-};
-
-export default DashboardProvider;
